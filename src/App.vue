9f2d505b5d11d2574019d1fa37fb8feac4a6df5c
Nomi <naumanahmed19@gmail.com>
2021-02-27 16:54:09 +0100

    enh(app): google font added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-
      <Home />
-  
 </template>
 
 <script setup>
 import Home from './components/Home.vue'
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
 </script>
 
 <style>
 @import'bootstrap/dist/css/bootstrap.css';
 
 #app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px; */
+font-family: 'Poppins', sans-serif;
 }
 
 </style>
