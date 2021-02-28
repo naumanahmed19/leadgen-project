535dc0cdfcef0e84ce85b2cc4e7f8782eaece90b
Nomi <naumanahmed19@gmail.com>
2021-02-28 19:02:36 +0100

    enh(app): router added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-     <Home />
+      <router-view/>
 </template>
 
 <script setup>
@@ -12,8 +12,5 @@ import Home from './components/Home.vue'
 #app {
 font-family: 'Poppins', sans-serif;
 }
-.form-select{
 
-  background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3e%3cpath fill='none' stroke='%23343a40' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M2 5l6 6 6-6'/%3e%3c/svg%3e")
-}
 </style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from '../components/Home.vue'
+import Exit from "../components/ExitPage.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/exit",
+    name: "Exit",
+    component: Exit,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+export default router
