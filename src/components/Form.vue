d308f29fad062ae894d1118e2d552e84f24f6c20
Nomi <naumanahmed19@gmail.com>
2021-02-28 01:16:00 +0100

    enh(home): form component updated

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,15 +1,140 @@
 <template>
- <div>
-   
- </div>
+  <div class="form-wrapper pb-5 my-5">
+    <div class="d-none d-sm-block d-md-block">
+      <Steps class="px-5 pt-5 pb-3 b-b" />
+    </div>
+    <div class="row px-5 ">
+      <div class="row  mt-5">
+        <div class="form-group">
+          <input type="text" class="form-control" id="usr" required />
+          <label for="usr">First Name</label>
+        </div>
+        <div class="form-group">
+          <input type="text" class="form-control" id="usr" required />
+          <label for="usr">Last Name</label>
+        </div>
+
+        <div class="form-group mb-2">
+          <input type="text" class="form-control" id="usr" required />
+          <label for="usr">Email</label>
+        </div>
+
+        <label for="usr">Date of birth: </label>
+        <div class="d-flex justify-content-between my-3">
+          <div class="form-group w-100">
+            <select class="form-select" aria-label="Date">
+              <option selected>Date</option>
+              <option :value="n" v-for="(n, index) in 31" :key="index">
+                {{ n }}
+              </option>
+            </select>
+          </div>
+          <div class="form-group w-100 mx-3">
+            <select class="form-select" aria-label="Date">
+              <option selected>Month</option>
+              <option :value="n" v-for="(n, index) in 12" :key="index">
+                {{ n }}
+              </option>
+            </select>
+          </div>
+          <div class="form-group w-100">
+            <select class="form-select" aria-label="Date">
+              <option selected>Year</option>
+              <option :value="n" v-for="(n, index) in 31" :key="index">
+                {{ n }}
+              </option>
+            </select>
+          </div>
+        </div>
+
+        <button
+          class="btn btn-primary btn-block rounded-2 r-5 btn-lg py-3 my-5"
+        >
+          ACCEPT T&C’S AND ENTER NOW
+        </button>
+
+        <div class="text-center">
+          <img src="../assets/images/verify.svg" alt="" />
+          Wij respecteren je privacy
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
 
-}
+<script setup>
+import { defineProps, reactive } from 'vue'
+import Footer from './Footer.vue'
+import Steps from './Steps.vue'
 </script>
+<style scoped lang="scss">
+.form-wrapper {
+  background: rgba(255, 255, 255, 0.08);
+  border-radius: 5px;
+  padding: auto;
+}
+.b-b {
+  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+}
+.btn-primary {
+  background: #0046a7;
+  border-radius: 30px !important;
+  border-color: #0046a7;
+  font-weight: bold;
+  font-size: 16px;
+}
 
-<style>
+.form-control,
+.form-select {
+  border: none;
+  border-bottom: 2px solid rgba(255, 255, 255, 0.3);
+  border-radius: 0;
+  padding-left: 0;
+  background-color: transparent;
+  color: #fff;
+}
+.form-control:focus {
+  color: #fff;
+  background-color: transparent;
+  border-color: #86b7fe;
+  outline: 0;
+  box-shadow: none;
+  border-bottom: 2px solid #fff;
+  padding-left: 0;
+}
 
+.form-group > label {
+  bottom: 34px;
+
+  position: relative;
+  background-color: transparent;
+  padding: 0;
+  font-size: 12px;
+  transition: 0.2s;
+  pointer-events: none;
+}
+
+.form-control:focus ~ label {
+  bottom: 55px;
+  padding: 0;
+}
+
+.form-control:valid ~ label {
+  bottom: 55px;
+}
+
+.form-select {
+  option {
+    color: #0046a7;
+  }
+  &::after {
+    background-image: url('../assets/images/arrow.svg') !important;
+    display: block;
+    width: 20px;
+    height: 20px;
+    color: red;
+    display: block;
+  }
+}
 </style>
