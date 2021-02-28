9e4076abb940123521c48b8a89e91b0af32b044c
Nomi <naumanahmed19@gmail.com>
2021-02-28 01:17:04 +0100

    fix(home): steps layout updated

---
+++ src/components/Steps.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="d-flex steps text-center justify-content-between">
+    <div class="text-center active-step">
+      <div class="number">1</div>
+      <p>
+        Beantwoord <br />
+        de vragen
+      </p>
+    </div>
+    <div>
+      <div class="number">2</div>
+      <p>
+        Vul je <br />
+        gegevens in
+      </p>
+    </div>
+    <div>
+      <div class="number">3</div>
+      <p>Heb je <br />gewonnen?</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import 'bootstrap/scss/bootstrap.scss';
+.steps {
+
+  p {
+    font-weight: 500;
+    font-size: 13px;
+  }
+  .number {
+    width: 50px;
+    height: 50px;
+    display: block;
+    background: #fff;
+    border-radius: 50%;
+    line-height: 40px;
+    color: #2b58bf;
+    margin: 0 auto 10px;
+    border: 5px solid #fff;
+    font-weight: 700;
+  }
+  .active-step {
+    color: #2b58bf;
+    .number {
+      color: #fff;
+      background-image: url('../assets/images/active.svg');
+      background: 100% 100%;
+      background-repeat: no-repeat;
+      background-size: cover;
+    }
+  }
+    @include media-breakpoint-down(sm) {
+    color:#434343
+    }
+}
+</style>
