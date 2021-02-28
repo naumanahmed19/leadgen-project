29f7804f94f13b996475b18ed4096f1a2727cf7a
Nomi <naumanahmed19@gmail.com>
2021-02-28 16:15:54 +0100

    enh(app): exit page components added

---
+++ src/components/Product.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="bg-white text-md-center box">
+    <h4 class="my-3">{{ item.title }}</h4>
+
+    <figure>
+      <img class="my-3" :src="item.image" alt="" />
+    </figure>
+    <a
+      v-if="item.url"
+      href="{{item.url}}"
+      class="btn btn-outline-primary btn-rounded px-3"
+    >
+      Kies deze
+      <img class="pl-3" src="../assets/icons/icon-arrow-left.svg" alt="" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import 'bootstrap/scss/bootstrap.scss';
+
+.box {
+  .btn-rounded {
+    border-color: #b4d1f8;
+    color: #2b58bf;
+    border-radius: 30px;
+    border-radius: 50px;
+    border-width: 2px;
+  }
+
+  padding: 3rem;
+  margin-bottom: 10px;
+  h4 {
+    font-size: 16px;
+    padding: 0 45px;
+  }
+  img {
+    max-height: 120px;
+  }
+}
+
+@media (max-width: 767px) {
+  figure {
+    position: absolute;
+    left: 20px;
+    top: 20px;
+    bottom: 0;
+  }
+  .box {
+    padding: 30px 0px 30px 170px;
+    h4 {
+      font-size: 20px;
+      padding: 0 0;
+    }
+    img {
+      max-height: 100px;
+    }
+  }
+}
+</style>
---
+++ src/components/ProductSmall.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="bg-white text-md-center box2">
+    <h4 class="my-3">{{ item.title }}</h4>
+    <figure>
+      <img  :src="item.image" alt="" />
+       <span class="time">{{item.time}}</span>
+    </figure>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import 'bootstrap/scss/bootstrap.scss';
+
+
+
+.box2 {
+  padding: 1rem 2rem;
+  margin-bottom: 10px;
+  border-radius: 6px;
+  h4 {
+    font-size: 14px;
+    padding: 0 10px;
+  }
+  figure {
+    position: relative;
+      img {
+        max-height: 100px;
+      }
+    }
+  .time {
+    border-color: 32px solid #b4d1f8;
+    color: #fff;
+    border-radius: 30px;
+    position: absolute;
+    background:#F44336;
+    font-size: 12px;
+    font-weight: bold;
+    padding: 8px;
+    left: 0;
+    right: 0;
+    
+    top: 50px;
+  }
+}
+  
+
+@media (max-width: 767px) {
+  
+  .box2 {
+    padding: 30px 0px 30px 200px;
+    figure {
+    position: absolute;
+    left: 40px;
+    top: 20px;
+    bottom: 0;
+      .time{
+      font-size: 10px;
+
+  }
+  
+ 
+  }
+
+  h4 {
+    font-size: 20px;
+    padding: 0 0;
+  }
+  }
+}
+</style>
